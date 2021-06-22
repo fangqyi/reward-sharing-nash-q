073029e987ddcfaaebebda35a4e88c910cfbb84c
QIAOYI FANG <QF30@duke.edu>
2021-06-22 15:21:33 -0400

    updated env init registry

--- src/env/__init__.py
+++ src/env/__init__.py
@@ -1,7 +1,14 @@
+from env.cleanup import Cleanup
+from env.escaperoom import EscapeRoom
+from env.ipd import IteratedPrisonerDilemma
 from env.unfair_game.gridmaze import GridMaze
 from env.unfair_game.surviving import Surviving
+
 
 REGISTRY = {}
 
 REGISTRY["gridmaze"] = GridMaze
 REGISTRY["surviving"] = Surviving
+REGISTRY["cleanup"] = Cleanup
+REGISTRY["escaperoom"] = EscapeRoom
+REGISTRY["ipd"] = IteratedPrisonerDilemma
