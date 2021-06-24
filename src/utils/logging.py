5564c823518e184700d96983d8e8ed593d13b40d
QIAOYI FANG <QF30@duke.edu>
2021-06-24 04:22:39 -0400

    debug prints

--- src/utils/logging.py
+++ src/utils/logging.py
@@ -56,6 +56,7 @@ class Logger:
                 continue
             i += 1
             window = 5 if k != "epsilon" else 1
+            print(k)
             item = "{:.4f}".format(np.mean([x[1] for x in self.stats[k][-window:]]))
             log_str += "{:<25}{:>8}".format(k + ":", item)
             log_str += "\n" if i % 4 == 0 else "\t"
