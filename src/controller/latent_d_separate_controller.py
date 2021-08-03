dd187f018b3b91866e13a868d8480b19a8c425a4
QIAOYI FANG <QF30@duke.edu>
2021-08-03 05:39:22 -0400

    added fully decentralized controller

---
+++ src/controller/latent_d_separate_controller.py
@@ -0,0 +1,194 @@
+import torch as th
+
+from components.action_selectors import EpsilonGreedyActionSelector
+from module.agents import REGISTRY as agent_REGISTRY
+
+
+# This multi-agent controller shares parameters between agents
+from module.utils import MLPMultiGaussianEncoder
+
+
+class SeparateLatentMAC:
+    def __init__(self, scheme, groups, args, train_phase):
+        self.n_agents = args.n_agents
+        self.train_phase = train_phase
+        self.args = args
+        self.scheme = scheme
+        self._build_agents()
+
+    def init_epsilon_schedule(self, phase):
+        self.action_selector = EpsilonGreedyActionSelector(self.args, phase)
+
+    def select_actions(self, ep_batch, t_ep, t_env, bs=slice(None), test_mode=False):
+        # Only select actions for the selected batch elements in bs
+        avail_actions = ep_batch["avail_actions"][:, t_ep]
+        agent_outputs = self.forward(ep_batch, t_ep, test_mode=test_mode)
+        chosen_actions = self.action_selector.select_action(agent_outputs[bs], avail_actions[bs], t_env, test_mode=test_mode)
+        return chosen_actions.tolist()
+
+    def sample_batch_latent_var(self, batch, t=None):
+        # infer z that encodes the information about the current reward-sharing scheme
+        # z_q, z_p: [n_agents][latent_relation_space_dim]
+        vars = []
+        for idx in range(self.n_agents):
+            inputs = self._build_latent_encoder_input(batch, t)
+            var = self.latent_encoders[idx].infer_posterior(inputs)
+            if len(var.shape) == 1:
+                var = var.unsqueeze(0)
+            vars.append(var)
+        return th.stack(vars, dim=1)
+
+    def sample_latent_var(self, z_q, z_p):
+        vars = []
+        for idx in range(self.n_agents):
+            inputs = th.cat([x.reshape(-1) for x in [z_q, z_p]], dim=-1).unsqueeze(0)
+            vars.append(self.latent_encoders[idx].infer_posterior(inputs))
+        return th.stack(vars, dim=1)
+
+    def compute_kl_div(self):
+        divs = []
+        for idx in range(self.n_agents):
+            divs.append(self.latent_encoders[idx].compute_kl_div())
+            self.latent_encoders[idx].reset()
+        return th.stack(divs, dim=0)
+
+    def forward(self, ep_batch, t, test_mode=False):
+        agent_inputs = self._build_inputs(ep_batch, t)
+        if self.args.is_obs_image:
+            obs_inputs, vec_inputs = agent_inputs
+        avail_actions = ep_batch["avail_actions"][:, t]
+        mask = ep_batch["adjacent_agents"][:, t]
+
+        agent_outs = []
+        for idx in range(self.n_agents):
+            if self.args.is_obs_image:
+                agent_input = [obs_inputs[idx], vec_inputs[idx]]
+            else:
+                agent_input = agent_inputs[idx]
+            if self.args.agent == "dgn_agent":
+                agent_out, self.hidden_states[idx] = self.agents[idx](agent_input, mask, self.hidden_states[idx])
+            else:
+                agent_out, self.hidden_states[idx] = self.agents[idx](agent_input, self.hidden_states[idx])
+            agent_outs.append(agent_out)
+        agent_outs = th.stack(agent_outs, dim=1).reshape(ep_batch.batch_size*self.n_agents, -1)
+
+        # Softmax the agent outputs if they're policy logits
+        if self.agent_output_type == "pi_logits":  # (0, 1) -> (-inf, inf)
+            if getattr(self.args, "mask_before_softmax", True):
+                # Make the logits for unavailable actions very negative to minimise their affect on the softmax
+                reshaped_avail_actions = avail_actions.reshape(ep_batch.batch_size * self.n_agents, -1)
+                agent_outs[reshaped_avail_actions == 0] = -1e10
+
+            agent_outs = th.nn.functional.softmax(agent_outs, dim=-1)
+            if not test_mode:
+                # Epsilon floor
+                epsilon_action_num = agent_outs.size(-1)
+                if getattr(self.args, "mask_before_softmax", True):
+                    # With probability epsilon, we will pick an available action uniformly
+                    epsilon_action_num = reshaped_avail_actions.sum(dim=1, keepdim=True).float()
+
+                agent_outs = ((1 - self.action_selector.epsilon) * agent_outs
+                               + th.ones_like(agent_outs) * self.action_selector.epsilon/epsilon_action_num)
+
+                if getattr(self.args, "mask_before_softmax", True):
+                    # Zero out the unavailable actions
+                    agent_outs[reshaped_avail_actions == 0] = 0.0
+
+        return agent_outs.view(ep_batch.batch_size, self.n_agents, -1)
+
+    def init_hidden(self, batch_size):
+        if 'init_hidden' in dir(self.agents[0]):
+            self.hidden_states = [self.agents[idx].init_hidden().unsqueeze(0).expand(batch_size, -1) for idx in range(self.n_agents)]
+
+    def parameters(self):
+        params = []
+        for idx in range(self.n_agents):
+            param = []
+            param.extend(list(self.agents[idx].parameters()))
+            param.extend(list(self.latent_encoders[idx].parameters()))
+            params.append(param)
+        return params
+
+    def load_state(self, other_mac):
+        for idx in range(self.n_agents):
+            self.agents[idx].load_state_dict(other_mac.agents[idx].state_dict())
+            self.latent_encoders[idx].load_state_dict(other_mac.latent_encoders[idx].state_dict())
+
+    def cuda(self):
+        for idx in range(self.n_agents):
+            self.agents[idx].cuda()
+            self.latent_encoders[idx].cuda()
+
+    def save_models(self, path):
+        for idx in range(self.n_agents):
+            th.save(self.agents[idx].state_dict(), "{}/agent{}.th".format(path, idx))
+            th.save(self.latent_encoders[idx].state_dict(), "{}/encoder{}.th".format(path, idx))
+
+    def load_models(self, path):
+        for idx in range(self.n_agents):
+            self.agents[idx].load_state_dict(th.load("{}/agent{}.th".format(path, idx), map_location=lambda storage, loc: storage))
+            self.latent_encoders[idx].load_state_dict(th.load("{}/encoder{}.th".format(path, idx), map_location=lambda storage, loc: storage))
+
+    def _build_agents(self):
+        self.agent_output_type = self.args.agent_output_type
+        self.agents = [agent_REGISTRY[self.args.agent](self.args, self.scheme) for _ in self.n_agents]
+        self.action_selectors = EpsilonGreedyActionSelector(self.args, self.train_phase)
+
+        latent_input_shape, latent_output_shape, latent_hidden_sizes = self._get_latent_shapes()
+        self.latent_encoders = [MLPMultiGaussianEncoder(latent_input_shape, latent_output_shape, latent_hidden_sizes)
+                                for _ in self.n_agents]
+        self.hidden_states = []
+
+    def _build_inputs(self, batch, t):
+        # Assumes homogenous agents with flat observations.
+        # Other MACs might want to e.g. delegate building inputs to each agent
+        bs = batch.batch_size
+        inputs = []
+        vec_inputs = []
+        if self.args.obs_last_action:
+            if t == 0:
+                vec_inputs.append(th.zeros_like(batch["actions_onehot"][:, t]))
+            else:
+                vec_inputs.append(batch["actions_onehot"][:, t-1])
+        vec_inputs.append(self.sample_batch_latent_var(batch, t))
+
+        # process observation
+        obs = batch["obs"][:, t]
+
+        if self.args.is_obs_image:
+            obs = obs.reshape(bs, self.n_agents, obs.shape[-3], obs.shape[-2], obs.shape[-1])  # flatten the first two dims
+            obs = th.split(obs, 1, dim=1)
+            vec_inputs = th.cat(vec_inputs, dim=-1)
+            vec_inputs = th.split(vec_inputs, 1, dim=1)
+            for _ in range(self.n_agents):
+                obs[_].squeeze(1)
+                vec_inputs[_].squeeze(1)
+            inputs = (obs, vec_inputs)  # return two objects as nn inputs
+        else:
+            inputs.append(obs)
+            inputs.extend(vec_inputs)
+            inputs = th.cat(inputs, dim=-1).split(1, dim=1)
+            for _ in range(self.n_agents):
+                inputs[_].squeeze(1)  # return one, catting obs and other agent info together
+        return inputs
+
+    def _get_latent_shapes(self):
+        # input shape: z, z' FIXME: could also use parsed relationship as input
+        latent_input_shape = self.args.latent_relation_space_dim*self.n_agents*2
+        latent_output_shape = self.args.latent_var_dim
+        latent_hidden_sizes = self.args.latent_encoder_hidden_sizes
+        return latent_input_shape, latent_output_shape, latent_hidden_sizes
+
+    def _build_latent_encoder_input(self, batch, t=None):
+        # z_q, z_p: [n_agents][latent_relation_space_dim]
+        bs = batch.batch_size
+        if t is None:
+            inputs = [batch["z_q"], batch["z_p"]]
+        elif len(batch["z_q"][:, t].shape) == 1:  # FIXME: probably unnecessary
+            inputs = [batch["z_q"][:, t].unsqueeze(0), batch["z_p"][:, t].unsqueeze(0)]
+        else:
+            inputs = [batch["z_q"][:, t], batch["z_p"][:, t]]
+
+        return th.cat([x.reshape(bs, -1) for x in inputs], dim=1)
+
+
--- src/learner/meta_learner.py
+++ src/learner/meta_learner.py
@@ -9,10 +9,10 @@ from torch.optim import Adam
 from module.critics.dist_critic import CentralizedDistCritic, DecentralizedDistCritic
 
 
-# centralized social welfare critic: all the agents optimize the estimated social utility
 class MetaQLearner:
     def __init__(self, mac, scheme, logger, args):
         self.args = args
+        self.n_agents = self.args.n_agents
         self.mac = mac
         self.logger = logger
 
@@ -21,6 +21,7 @@ class MetaQLearner:
         self.last_target_update_episode = 0
 
         if args.centralized_social_welfare:
+            # centralized social welfare critic: all the agents optimize the estimated social utility
             self.z_critic = CentralizedDistCritic(scheme, args)
             self.z_critic_params = list(self.z_critic.parameters())
             self.z_critic_optimiser = Adam(params=self.z_critic_params, lr=args.z_critic_lr,
@@ -31,7 +32,11 @@ class MetaQLearner:
             self.z_critic_params = sum([list(critic.parameters()) for critic in self.z_critics], [])
             self.z_critic_optimiser = Adam(params=self.z_critic_params, lr=args.z_critic_lr, eps=args.optim_eps)
 
-        self.optimiser = Adam(params=self.params, lr=args.lr, eps=args.optim_eps)
+        if args.separate_agents:
+            self.optimisers = [Adam(params=self.params[idx], lr=args.lr, eps=args.optim_eps) for idx in range(self.n_agents)]
+        else:
+            self.optimiser = Adam(params=self.params, lr=args.lr, eps=args.optim_eps)
+
         # betas=(args.optim_beta1, args.optim_beta2)
         # a little wasteful to deepcopy (e.g. duplicates action selector), but should work for any MAC
         self.target_mac = copy.deepcopy(mac)
@@ -69,6 +74,12 @@ class MetaQLearner:
             self.log_stats_t = t_env
 
     def train(self, batch: EpisodeBatch, t_env: int, episode_num: int):
+        if self.args.separate_agents:
+            self._separate_train(batch, t_env, episode_num)
+        else:
+            self._train(batch, t_env, episode_num)
+
+    def _train(self, batch: EpisodeBatch, t_env: int, episode_num: int):
         # Get the relevant quantities
         rewards = batch["redistributed_rewards"][:, :-1]
         actions = batch["actions"][:, :-1]
@@ -154,6 +165,96 @@ class MetaQLearner:
             self.logger.log_stat("target_mean", (targets * mask).sum().item()/(mask_elems * self.args.n_agents), t_env)
             self.log_stats_t = t_env
 
+    def _separate_train(self, batch: EpisodeBatch, t_env: int, episode_num: int):
+        # Get the relevant quantities
+        rewards = batch["redistributed_rewards"][:, :-1]  # [bs, t, n_agents, -1]
+        actions = batch["actions"][:, :-1]  # [bs, t, n_agents, -1]
+        terminated = batch["terminated"][:, :-1].float()
+        mask = batch["filled"][:, :-1].float()
+        mask[:, 1:] = mask[:, 1:] * (1 - terminated[:, :-1])
+        avail_actions = batch["avail_actions"]  # [bs, t, n_agents, -1]
+
+        # Calculate estimated Q-Values
+        mac_out = []
+        self.mac.init_hidden(batch.batch_size)
+        # self.mac.init_latent(batch.batch_size)
+
+        kl_divs = []
+        for t in range(batch.max_seq_length):
+            agent_outs = self.mac.forward(batch, t=t)  # (bs,n,n_actions)
+            kl_divs.append(self.mac.compute_kl_div())  # (bs,n, ))
+            mac_out.append(agent_outs)  # [t,(bs,n,n_actions)]
+        mac_out = th.stack(mac_out, dim=1)  # Concat over time
+        kl_divs = th.stack(kl_divs, dim=1)[:, :-1]
+        # (bs,t,n,n_actions), Q values of n_actions
+
+        # Pick the Q-Values for the actions taken by each agent
+        chosen_action_qvals = th.gather(mac_out[:, :-1], dim=3, index=actions).squeeze(3)  # Remove the last dim
+        # (bs,t,n) Q value of an action
+
+        # Calculate the Q-Values necessary for the target
+        target_mac_out = []
+        self.target_mac.init_hidden(batch.batch_size)  # (bs,n,hidden_size)
+        # self.target_mac.init_latent(batch.batch_size)
+
+        for t in range(batch.max_seq_length):
+            target_agent_outs = self.target_mac.forward(batch, t=t)  # (bs,n,n_actions)
+            target_mac_out.append(target_agent_outs)  # [t,(bs,n,n_actions)]
+
+        # We don't need the first timesteps Q-Value estimate for calculating targets
+        target_mac_out = th.stack(target_mac_out[1:], dim=1)  # Concat across time, dim=1 is time index
+        # (bs,t,n,n_actions)
+
+        # Mask out unavailable actions
+        target_mac_out[avail_actions[:, 1:] == 0] = -9999999  # Q values
+
+        target_max_qvals = target_mac_out.max(dim=3)[0]
+
+        # Calculate 1-step Q-Learning targets
+        targets = rewards + self.args.gamma * (1 - terminated) * target_max_qvals
+
+        # Td-error
+        td_error = (chosen_action_qvals - targets.detach())  # no gradient through target net
+        # (bs,t,1)
+
+        kl_divs = kl_divs.split(1, dim=2)
+        td_errors = td_error.split(1, dim=2)
+        for idx in range(self.n_agents):
+            kl_mask = copy.deepcopy(mask).expand_as(kl_divs[idx])
+            masked_kl_div = kl_divs * kl_mask
+            kl_div_loss = masked_kl_div.sum() / kl_mask.sum()
+
+            mask = mask.expand_as(td_errors[idx])
+            # 0-out the targets that came from padded data
+            masked_td_error = td_errors[idx] * mask
+            # Normal L2 loss, take mean over actual data (LSE)
+            td_error_loss = (masked_td_error ** 2).sum() / mask.sum()
+
+            loss = td_error_loss + kl_div_loss
+
+            # Optimise
+            self.optimisers[idx].zero_grad()
+            loss.backward()
+            grad_norm = th.nn.utils.clip_grad_norm_(self.params[idx], self.args.grad_norm_clip)  # max_norm
+            self.optimisers[idx].step()
+
+            if t_env - self.log_stats_t >= self.args.learner_log_interval:
+                self.logger.log_stat("loss_{}".format(idx), loss.item(), t_env)
+                self.logger.log_stat("policy_grad_norm_{}".format(idx), grad_norm, t_env)
+                mask_elems = mask.sum().item()
+                self.logger.log_stat("kl_div_abs_{}".format(idx), kl_div_loss.abs().item(), t_env)
+                self.logger.log_stat("td_error_abs_{}".format(idx), (masked_td_error.abs().sum().item() / mask_elems), t_env)
+                self.logger.log_stat("q_taken_mean_{}".format(idx),
+                                     (chosen_action_qvals * mask).sum().item() / (mask_elems * self.args.n_agents),
+                                     t_env)
+                self.logger.log_stat("target_mean_{}".format(idx), (targets * mask).sum().item() / (mask_elems * self.args.n_agents),
+                                     t_env)
+                self.log_stats_t = t_env
+
+        if (episode_num - self.last_target_update_episode) / self.args.target_update_interval >= 1.0:
+            self._update_targets()
+            self.last_target_update_episode = episode_num
+
     def _update_targets(self):
         self.target_mac.load_state(self.mac)
         self.logger.console_logger.info("Updated target network")
@@ -169,10 +270,19 @@ class MetaQLearner:
 
     def save_models(self, path):
         self.mac.save_models(path)
-        th.save(self.optimiser.state_dict(), "{}/opt.th".format(path))
+        if self.args.separate_agents:
+            for idx in range(self.n_agents):
+                th.save(self.optimisers[idx].state_dict(), "{}/opt{}.th".format(path, idx))
+        else:
+            th.save(self.optimiser.state_dict(), "{}/opt.th".format(path))
 
     def load_models(self, path):
         self.mac.load_models(path)
         # Not quite right but I don't want to save target networks
         self.target_mac.load_models(path)
-        self.optimiser.load_state_dict(th.load("{}/opt.th".format(path), map_location=lambda storage, loc: storage))
+        if self.args.separate_agents:
+            for idx in range(self.n_agents):
+                self.optimisers[idx].load_state_dict(
+                    th.load("{}/opt{}.th".format(path, idx), map_location=lambda storage, loc: storage))
+        else:
+            self.optimiser.load_state_dict(th.load("{}/opt.th".format(path), map_location=lambda storage, loc: storage))
