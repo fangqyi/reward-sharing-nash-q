9b5c0b31cd715f87b401f501d8e4fba0cfbe1d52
QIAOYI FANG <QF30@duke.edu>
2021-08-03 05:47:07 -0400

    added configs

--- src/controller/__init__.py
+++ src/controller/__init__.py
@@ -2,10 +2,11 @@ from controller.basic_pq_shared_controller import BasicPQMAC
 from controller.latent_d_shared_controller import BasicLatentMAC
 from controller.shared_controller import BasicMAC
 from controller.shared_controller_nash_q import SharedNashMAC
-
+from controller.latent_d_separate_controller import SeparateLatentMAC
 REGISTRY = {}
 
 REGISTRY["shared_nash"] = SharedNashMAC
 REGISTRY["shared"] = BasicMAC
 REGISTRY["shared_pq"] = BasicPQMAC
 REGISTRY["latent_dist"] = BasicLatentMAC
+REGISTRY["separate_dist"] = SeparateLatentMAC
