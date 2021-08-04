4a7e1286f1b8aaed2c6fca4ee748a6c66e9f9d15
QIAOYI FANG <QF30@duke.edu>
2021-08-04 06:22:16 -0400

    fixing retrainable bug

--- src/learner/meta_learner.py
+++ src/learner/meta_learner.py
@@ -244,7 +244,7 @@ class MetaQLearner:
 
             # Optimise
             self.optimisers[idx].zero_grad()
-            loss.backward(allow_unreachable=True)
+            loss.backward(retain_graph=True)
             grad_norm = th.nn.utils.clip_grad_norm_(self.params[idx], self.args.grad_norm_clip)  # max_norm
             self.optimisers[idx].step()
 
