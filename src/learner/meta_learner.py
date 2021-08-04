2cd434db6c81a185b75e579a7d158f4b707fbbb7
QIAOYI FANG <QF30@duke.edu>
2021-08-04 03:43:33 -0400

    added prints

--- src/learner/meta_learner.py
+++ src/learner/meta_learner.py
@@ -224,6 +224,10 @@ class MetaQLearner:
         for idx in range(self.n_agents):
             kl_divs[idx] = kl_divs[idx].squeeze(2)
             td_errors[idx] = td_errors[idx].squeeze(2)
+            print("mask shape")
+            print(mask)
+            print("kl_divs")
+            print(kl_divs[idx].shape)
             kl_mask = copy.deepcopy(mask).expand_as(kl_divs[idx])
             masked_kl_div = kl_divs * kl_mask
             kl_div_loss = masked_kl_div.sum() / kl_mask.sum()
