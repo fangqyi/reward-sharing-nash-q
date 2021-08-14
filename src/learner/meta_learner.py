e8be1a555945c8272eeb194fb523dab4d73fe2c9
QIAOYI FANG <QF30@duke.edu>
2021-08-14 02:22:47 -0400

    implemented no sharing scheme encoder

--- src/learner/meta_learner.py
+++ src/learner/meta_learner.py
@@ -186,15 +186,18 @@ class MetaQLearner:
         for t in range(batch.max_seq_length):
             agent_out = self.mac.forward_agent(batch, idx=idx, t=t)  # (bs,n_actions)
             _ = self.mac.forward_inference_net_agent(idx=idx)
-            kl_div = self.mac.compute_kl_div_agent(idx=idx)
-            if self.args.mutual_information_reinforcement:
-                kl_div, ce_loss = kl_div
-                ce_losses.append(ce_loss)
-            kl_divs.append(kl_div)  # (bs, ))
+            if self.args.sharing_scheme_encoder:
+                kl_div = self.mac.compute_kl_div_agent(idx=idx)
+                if self.args.mutual_information_reinforcement:
+                    kl_div, ce_loss = kl_div
+                    ce_losses.append(ce_loss)
+                kl_divs.append(kl_div)  # (bs, ))
             mac_out.append(agent_out)  # [t,(bs,n_actions)]
         mac_out = th.stack(mac_out, dim=1)  # Concat over time
-        kl_divs = th.stack(kl_divs, dim=1)[:, :-1]
-        ce_losses = th.stack(ce_losses, dim=1)[:, :-1]
+        if self.args.sharing_scheme_encoder:
+            kl_divs = th.stack(kl_divs, dim=1)[:, :-1]
+            if self.args.mutual_information_reinforcement:
+                ce_losses = th.stack(ce_losses, dim=1)[:, :-1]
         # (bs,t,n,n_actions), Q values of n_actions
 
         # Pick the Q-Values for the actions taken by each agent
@@ -226,21 +229,24 @@ class MetaQLearner:
         td_error = (chosen_action_qvals - targets.detach()).unsqueeze(-1)  # no gradient through target net
         # (bs,t,1)
 
-        kl_mask = copy.deepcopy(mask).expand_as(kl_divs)
-        masked_kl_div = kl_divs * kl_mask
-        kl_div_loss = masked_kl_div.sum() / kl_mask.sum()
-
-        ce_mask = copy.deepcopy(mask).expand_as(ce_losses)
-        masked_ce_loss = ce_losses * ce_mask
-        ce_losses = masked_ce_loss.sum() / ce_mask.sum()
-
         td_mask = copy.deepcopy(mask).expand_as(td_error)
         # 0-out the targets that came from padded data
         masked_td_error = td_error * td_mask
         # Normal L2 loss, take mean over actual data (LSE)
         td_error_loss = (masked_td_error ** 2).sum() / td_mask.sum()
+        loss = td_error_loss
 
-        loss = td_error_loss + kl_div_loss + ce_losses
+        if self.args.sharing_scheme_encoder:
+            kl_mask = copy.deepcopy(mask).expand_as(kl_divs)
+            masked_kl_div = kl_divs * kl_mask
+            kl_div_loss = masked_kl_div.sum() / kl_mask.sum()
+            loss += kl_div_loss
+
+        if self.args.mutual_information_reinforcement:
+            ce_mask = copy.deepcopy(mask).expand_as(ce_losses)
+            masked_ce_loss = ce_losses * ce_mask
+            ce_losses = masked_ce_loss.sum() / ce_mask.sum()
+            loss += ce_losses
 
         # Optimise
         self.optimisers[idx].zero_grad()
@@ -252,8 +258,10 @@ class MetaQLearner:
             self.logger.log_stat("agent{}_loss".format(idx), loss.item(), t_env)
             self.logger.log_stat("agent{}_policy_grad_norm".format(idx), grad_norm, t_env)
             mask_elems = td_mask.sum().item()
-            self.logger.log_stat("agent{}_kl_div_abs".format(idx), kl_div_loss.abs().item(), t_env)
-            self.logger.log_stat("agent{}_ce_loss_abs".format(idx), ce_losses.abs().item(), t_env)
+            if self.args.sharing_scheme_encoder:
+                self.logger.log_stat("agent{}_kl_div_abs".format(idx), kl_div_loss.abs().item(), t_env)
+            if self.args.mutual_information_reinforcement:
+                self.logger.log_stat("agent{}_ce_loss_abs".format(idx), ce_losses.abs().item(), t_env)
             self.logger.log_stat("agent{}_td_error_abs".format(idx), (masked_td_error.abs().sum().item() / mask_elems), t_env)
             self.logger.log_stat("agent{}_target_mean".format(idx), (targets.unsqueeze(-1) * td_mask).sum().item() / (mask_elems ),
                                  t_env)
