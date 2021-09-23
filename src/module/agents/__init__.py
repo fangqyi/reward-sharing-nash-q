985830bb38e09ec0b05124a43796095ebb4a4f79
QIAOYI FANG <QF30@duke.edu>
2021-09-23 03:55:38 -0400

    removed outdated code

--- src/module/agents/__init__.py
+++ src/module/agents/__init__.py
@@ -1,12 +1,8 @@
 from module.agents.dgn_agent import DGNAgent
-from module.agents.junk_agent import PQAgent
-from module.agents.nash_q_agent import NashQAgent
 from module.agents.rnn_agent import RNNAgent, RNNAgentImageVec
 
 REGISTRY = {}
 
-REGISTRY["nash_q_agent"] = NashQAgent
 REGISTRY["rnn_agent"] = RNNAgent
 REGISTRY["dgn_agent"] = DGNAgent
-REGISTRY["pq_agent"] = PQAgent
 REGISTRY["rnn_agent_image_vec"] = RNNAgentImageVec
