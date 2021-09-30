1ef467bb990b5d615dc9b3016b3e62366aabf204
QIAOYI FANG <QF30@duke.edu>
2021-09-30 00:19:38 -0400

    debugged

--- src/controller/z_q_separate_controller.py
+++ src/controller/z_q_separate_controller.py
@@ -41,7 +41,11 @@ class ZQSeparateMAC():
         self.z_q_actors_selector = EpsilonGreedyActionSelector(self.args, "z_train")
 
     def forward_agent(self, data, idx, is_train):  # should only be used in training
-        z_q_vals = self._forward_z_q(data, idx, data["cur_z_p"] if is_train else None, is_train)
+        if is_train:
+            cur_z_p = data["cur_z_p"]
+        else:
+            cur_z_p = None
+        z_q_vals = self._forward_z_q(data, idx, cur_z_p, is_train)
         if idx < self.args.n_agents - 1:
             z_p_vals = self._forward_z_p(data, idx)
         else:
@@ -68,7 +72,7 @@ class ZQSeparateMAC():
 
     def select_z_agent(self, data, t_env, idx, test_mode=False):
         # z_p
-        z_p_outs, z_q_outs = self.forward_agent(data, idx, is_train="False")
+        z_p_outs, z_q_outs = self.forward_agent(data, idx, is_train=False)
         z_p_outs = z_p_outs.view(1, self.args.latent_relation_space_dim, -1)
         z_q_outs = z_q_outs.view(1, self.args.latent_relation_space_dim, -1)
 
