46ef39b085601ab7e804d826ffdc20cf161c1003
QIAOYI FANG <QF30@duke.edu>
2021-09-30 00:28:05 -0400

    debugged

--- src/controller/z_q_separate_controller.py
+++ src/controller/z_q_separate_controller.py
@@ -72,18 +72,19 @@ class ZQSeparateMAC():
 
     def select_z_agent(self, data, t_env, idx, test_mode=False):
         # z_p
-        z_p_outs, z_q_outs = self.forward_agent(data, idx, is_train=False)
+        z_p_outs = self._forward_z_p(data, idx)
         z_p_outs = z_p_outs.view(1, self.args.latent_relation_space_dim, -1)
-        z_q_outs = z_q_outs.view(1, self.args.latent_relation_space_dim, -1)
 
         if idx != self.n_agents-1:
             chosen_z_p_idx = self.z_p_actors_selector.select_action(z_p_outs, t_env=t_env, test_mode=test_mode).view(-1)
             z_p = th.tensor([self.z_options[chosen_z_p_idx[idx]] for idx in range(len(chosen_z_p_idx))]).float().to(self.args.device)
         else:  # fixed the value for last agent
             chosen_z_p_idx = th.zeros([self.args.latent_relation_space_dim])
-            z_p = th.zeros([self.args.latent_relation_space_dim])
+            z_p = th.zeros([self.args.latent_relation_space_dim]).float()
 
         # z_q
+        z_q_outs = self._forward_z_q(data, idx, z_p, is_train=False)
+        z_q_outs.view(1, self.args.latent_relation_space_dim, -1)
         chosen_z_q_idx = self.z_q_actors_selector.select_action(z_q_outs, t_env=t_env, test_mode=test_mode).view(-1)
         z_q = th.tensor([self.z_options[chosen_z_q_idx[idx]] for idx in range(len(chosen_z_q_idx))]).float()
 
